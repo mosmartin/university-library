247c31630edd2a30c4b3309236e916114c65cf3e
Martin Macharia <mosmartin5@gmail.com>
2025-06-01 00:34:30 +0200

    feat: add new SVG icons for logout, receipt, search, star, tick, upload, user, verified, and warning; include new images for auth illustration and no books; remove unused SVGs; update layout to include Header component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+
 export default function Layout({
   children,
 }: Readonly<{
@@ -6,7 +8,8 @@ export default function Layout({
   return (
     <main className="flex min-h-screen flex-1 flex-col bg-top bg-dark-100 px-5 xs:px-10 md:px-16">
       <div className="mx-auto max-w-7xl">
-        Header
+        <Header />
+
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
